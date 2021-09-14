ae13243bdef2411556c846dbec98ef147e427af5
youxuehu <youxuehu@hotmail.com>
2021-09-14 21:43:13 +0800

    nbconvert TigerPreprocessor 拆成2哥文件，新版走V2,旧版走V1,互不干涉

--- JupyterNotebookBase/public_extensions/tiger_preprocessor_v2.py
+++ JupyterNotebookBase/public_extensions/tiger_preprocessor_v2.py
@@ -13,14 +13,23 @@ class TigerPreprocessorV2(ExecutePreprocessor):
 
     def preprocess(self, nb, resources=None, km=None):
         print("param_file_path: %s" % self.param_file_path)
-        print("================TigerPreprocessorV2=============preprocess============================================")
+        print("================TigerPreprocessorV2=============preprocess========================================")
         return super(TigerPreprocessorV2, self).preprocess(nb, resources, km)
 
     def preprocess_cell(self, cell, resources, index):
-        print("================TigerPreprocessorV2=============preprocess_cell========================================")
+        print("================TigerPreprocessorV2=============preprocess_cell===================================")
         cell, resources = super(TigerPreprocessorV2, self).preprocess_cell(cell, resources, index)
-        new_cell = self.execute_cell(cell=new_code_cell(source="ls"), cell_index=index, store_history=True)
-        print(new_cell)
+        new_cell = self.execute_cell(cell=new_code_cell(source="%who"), cell_index=index, store_history=True)
+        print()
+        print(new_cell.outputs[0]['text'])
+        print()
+        if new_cell.outputs[0]['text']:
+            for v in new_cell.outputs[0]['text'].split("\t"):
+                try:
+                    msg = self.execute_cell(cell=new_code_cell(source="%store {}".format(v.strip())), cell_index=index, store_history=True).outputs[0]['text'] # noqa
+                    print(msg)
+                except Exception: # noqa
+                    pass
         self.nb['cells'][index] = cell
         return cell, resources
 
