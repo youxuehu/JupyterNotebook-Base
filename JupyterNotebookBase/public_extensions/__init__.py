e779af99f2c89a71b53060d4b721d981821313e9
youxuehu <youxuehu@hotmail.com>
2021-12-25 14:56:18 +0800

    tensorflow helloworld

--- JupyterNotebookBase/public_extensions/__init__.py
+++ JupyterNotebookBase/public_extensions/__init__.py
@@ -4,6 +4,6 @@ import os
 
 current_dir = os.path.dirname(__file__)
 print(current_dir)
-monitor_base_path = "/tmp/monitor"
-monitor_log_path = "/root/monitor_event_%s.log"
-monitor_pid_path = "/root/monitor_event_%s.pid"
+monitor_base_path = "~/monitor"
+monitor_log_path = "~/monitor_event_%s.log"
+monitor_pid_path = "~/monitor_event_%s.pid"
