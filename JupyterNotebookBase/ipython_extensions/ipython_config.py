cf398222381eb8b1f54aaef2753bc8d720f9e108
youxuehu <youxuehu@hotmail.com>
2022-03-26 19:47:56 +0800

    fix

--- JupyterNotebookBase/ipython_extensions/ipython_config.py
+++ JupyterNotebookBase/ipython_extensions/ipython_config.py
@@ -12,5 +12,7 @@ if sys.platform == "linux":
     os.environ["IPYTHON_PROFILE_PATH"] = "/root/.ipython/profile_default"
 elif sys.platform == "darwin":
     os.environ["IPYTHON_PROFILE_PATH"] = "%s/.ipython/profile_default" % os.getenv("HOME")
+if not os.path.exists(os.environ["IPYTHON_PROFILE_PATH"]):
+    os.makedirs(os.environ["IPYTHON_PROFILE_PATH"])
 with codecs.open(os.path.join(os.getenv("IPYTHON_PROFILE_PATH"), "ipython_config.py"), "w") as fout:
     fout.write("\n".join(lines))
